9158bf305e306364be92be9ec7949ca1fa6cc261
Lisa Caputo <lcaputo7788@yahoo.com>
2022-07-19 21:54:36 -0400

    connect axios and start to attempt getting data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useState, useEffect } from 'react'
+import { BASE_URL } from './global'
+import BrandList from './components/BrandList'
+import axios from 'axios'
 import './styles/App.css'
 
 const App = () => {
+  useEffect(()=>{
+    //this will happen right when the component loads
+    const getBrands = async () => {
+      //const brand = 
+      //endpoint
+      const response = await axios.get(`${BASE_URL}`)
+      //console.log(response);
+      console.log(response.data);
+    }
+    getBrands()
+  }, [])
+
+
   return (
     <div>
 
---
+++ src/components/BrandList.jsx
@@ -0,0 +1,15 @@
+const BrandList = (props) => {
+
+    return (
+        <div>
+            {/* {props.brands.map((brand) => (
+                <div key={brand.id} className="hello">
+            <h1>{brand.title}</h1>
+            ))
+
+            } */}
+        </div>
+    )
+}
+
+export default BrandList
