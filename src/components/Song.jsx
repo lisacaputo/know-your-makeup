811ebd567377436f4f4f9dfa6d3c5fe0e76ea939
Lisa Caputo <lcaputo7788@yahoo.com>
2022-07-21 14:16:03 -0400

    Add release date of single

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -2,12 +2,9 @@ const Song = (props) => {
 
     return (
         <div className="songCard">
-
             <img src={props.song.song_art_image_thumbnail_url} alt="song artwork" />
-            <h2>Song Title: {props.song.title}</h2>
-
-
-
+            <h2>Song Title: {props.song.title_with_featured}</h2>
+            <span>{props.song.release_date_for_display}</span>
         </div>
 
     )
